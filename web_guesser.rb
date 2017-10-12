ed9c88d10a42ce6e4ba47946a8a8568045cec0b8
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-10-11 19:26:03 -0500

    finish I5

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,34 +1,53 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
+@@guesses_left = 5
+
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
   color = color_choice(guess)
-  erb :index, locals: { number: SECRET_NUMBER, message: message, color: color }
+  @@guesses_left -= 1
+  number = select_number
+  message = @@guesses_left.zero? ? reset : check_guess(guess)
+  puts @@guesses_left
+  erb :index, locals: { number: number, message: message, color: color }
+end
+
+def select_number
+  return @@secret_number if params['guess'] == @@secret_number ||
+                            params['cheat'] == true ||
+                            @@guesses_left.zero?
 end
 
 def check_guess(guess)
-  if guess < SECRET_NUMBER
-    return 'Way Too Low!' if guess < SECRET_NUMBER - 5
+  if guess < @@secret_number
+    return 'Way Too Low!' if guess < @@secret_number - 5
     'Too Low!'
-  elsif guess > SECRET_NUMBER
-    return 'Way Too High!' if guess > SECRET_NUMBER + 5
+  elsif guess > @@secret_number
+    return 'Way Too High!' if guess > @@secret_number + 5
     'Too High!'
   else
-    'You got it right!'
+    reset
+    "You're Right! Resetting..."
   end
 end
 
 def color_choice(guess)
-  if guess < SECRET_NUMBER
-    return 'red' if guess < SECRET_NUMBER - 5
+  if guess < @@secret_number
+    return 'red' if guess < @@secret_number - 5
     '#ff5050'
-  elsif guess > SECRET_NUMBER
-    return 'red' if guess > SECRET_NUMBER + 5
+  elsif guess > @@secret_number
+    return 'red' if guess > @@secret_number + 5
     '#ff5050'
   else
     'green'
   end
+end
+
+def reset
+  fail = @@guesses_left.zero?
+  @@guesses_left = 5
+  @@secret_number = rand(100)
+  'Resetting' if fail
 end
